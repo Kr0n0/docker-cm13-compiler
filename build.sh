aa69ef13d494ac2e9ac3a9bdcc5c4fe7d9b6fdae
Kr0n0 <ccvals@linux.com>
2016-05-11 12:07:50 +0200

    Cambios en script de build
    Cambios en script de build para comentar prebuilts (en CM13 van con
    breakfast si no las detecta) y configuración de git para modo batch de
    repo

--- build.sh
+++ build.sh
@@ -1,7 +1,10 @@
 #!/bin/sh -e
-repo init -u git://github.com/CyanogenMod/android.git -b cm-13.0
+git config --global user.email "carlos@caseonit.net"
+git config --global user.name "Carlos Crisostomo"
+git config --global color.ui true
+repo --color=always init -u git://github.com/CyanogenMod/android.git -b cm-13.0
 repo sync
-cd system/vendor/cm && ./get-prebuilts
+#cd system/vendor/cm && ./get-prebuilts
 source build/envsetup.sh
 breakfast bullhead
 brunch bullhead
